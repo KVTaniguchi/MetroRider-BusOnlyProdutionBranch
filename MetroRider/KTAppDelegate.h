a5538a1c5ba1631be8c54e3a6fcd45117749c3d4
KVTaniguchi <kv.taniguchi@gmail.com>
2014-06-08 13:45:21 -0400

    removed *****

--- MetroRider/KTAppDelegate.h
+++ MetroRider/KTAppDelegate.h
@@ -5,7 +5,6 @@
 //  Created by Kevin Taniguchi on 4/7/14.
 //  Copyright (c) 2014 Taniguchi. All rights reserved.
 //
-// ****
 #import <UIKit/UIKit.h>
 
 @interface KTAppDelegate : UIResponder <UIApplicationDelegate>
